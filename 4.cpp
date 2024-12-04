e3c84b7306628b0b0dcf9045e3b66a3c79c0c873
Myles Hyson <myleshyson@ufl.edu>
2024-12-04 15:46:38 -0800

    use iterative solution for question 4.

--- 4.cpp
+++ 4.cpp
@@ -3,29 +3,6 @@
 #include <iostream>
 #include "helpers.h"
 
-// xmas
-bool dfs(
-    std::pair<int, int> node,
-    const std::map<std::pair<int, int>, std::vector<std::pair<int, int>>> &graph,
-    const std::vector<std::vector<char>> &input,
-    std::set<std::pair<int, int>> &seen,
-    std::pair<int, int> direction,
-    const char letter,
-    const std::vector<char> &letters) {
-
-    if (input[node.first][node.second] != letters[letter]) return false;
-    if (letter == letters.size() - 1 && input[node.first][node.second] == letters[letter]) return true;
-
-    for (auto &neighbor: graph.at(node)) {
-        if (!seen.contains(neighbor) && std::make_pair(node.first + direction.first, node.second + direction.second) == neighbor) {
-            seen.insert(neighbor);
-            return dfs(neighbor, graph, input, seen, direction, letter + 1, letters);
-        }
-    }
-
-    return false;
-}
-
 int first() {
     int answer = 0;
     std::ifstream file = get_input("4.txt");
@@ -53,9 +30,13 @@ int first() {
         input.push_back(row);
     }
 
+    std::vector letters = {'X', 'M', 'A', 'S'};
+
+    int curLetter = 0;
+
     for (int i = 0; i < input.size(); i++) {
         for (int j = 0; j < input[i].size(); j++) {
-            std::vector<std::pair<int, int> > neighbors;
+            if (input[i][j] != letters[curLetter]) continue;
 
             for (auto direction: directions) {
                 int newRow = i + direction.first;
@@ -65,27 +46,25 @@ int first() {
                     continue;
                 }
 
-                neighbors.emplace_back(newRow, newCol);
+                curLetter++;
+
+                while (curLetter < 4 && input[newRow][newCol] == letters[curLetter]) {
+                    newRow = newRow + direction.first;
+                    newCol = newCol + direction.second;
+
+
+                    curLetter++;
+
+                    if (newRow < 0 || newRow >= input.size() || newCol < 0 || newCol >= input[i].size()) {
+                        break;
+                    }
+                }
+
+                if (curLetter == 4) answer++;
+
+                curLetter = 0;
             }
-
-            graph.emplace(std::make_pair(i, j), neighbors);
         }
-    }
-
-    std::vector letters = {'X', 'M', 'A', 'S'};
-
-    for (const auto &pair: graph) {
-        std::pair<int, int> node = pair.first;
-
-        if (input[node.first][node.second] != 'X') continue;
-
-        for (auto direction : directions) {
-            std::set<std::pair<int, int>> seen;
-            if (dfs(node, graph, input, seen, direction, 0, letters)) {
-                answer++;
-            }
-        }
-
     }
 
     return answer;
@@ -114,11 +93,20 @@ int second() {
         input.push_back(row);
     }
 
+    std::vector letters = {'M', 'A', 'S'};
+    std::set<std::pair<int, int>> intersectingPoints;
+    int curLetter = 0;
+
     for (int i = 0; i < input.size(); i++) {
         for (int j = 0; j < input[i].size(); j++) {
-            std::vector<std::pair<int, int> > neighbors;
+
+            if (input[i][j] != letters[curLetter]) continue;
 
             for (auto direction: directions) {
+                std::set<std::pair<int, int>> seen;
+
+                seen.emplace(i, j);
+
                 int newRow = i + direction.first;
                 int newCol = j + direction.second;
 
@@ -126,35 +114,31 @@ int second() {
                     continue;
                 }
 
-                neighbors.emplace_back(newRow, newCol);
-            }
+                curLetter++;
 
-            graph.emplace(std::make_pair(i, j), neighbors);
-        }
-    }
+                while (curLetter < 3 && input[newRow][newCol] == letters[curLetter]) {
+                    seen.emplace(newRow, newCol);
+                    newRow = newRow + direction.first;
+                    newCol = newCol + direction.second;
 
-    std::vector letters = {'M', 'A', 'S'};
-    std::set<std::pair<int, int>> intersectingPoints;
 
-    for (const auto &pair: graph) {
-        std::pair<int, int> node = pair.first;
+                    curLetter++;
 
-        if (input[node.first][node.second] != 'M') continue;
-
-        for (auto direction : directions) {
-            std::set<std::pair<int, int>> seen;
-
-            if (dfs(node, graph, input, seen, direction, 0, letters)) {
-                for (auto point : seen) {
-                    if (intersectingPoints.contains(point) && input[point.first][point.second] == 'A') answer++;
-
-                    if (!intersectingPoints.contains(point)) {
-                        intersectingPoints.insert(point);
+                    if (newRow < 0 || newRow >= input.size() || newCol < 0 || newCol >= input[i].size()) {
+                        break;
                     }
                 }
+
+                if (curLetter == 3) {
+                    for (auto point : seen) {
+                        if (intersectingPoints.contains(point) && input[point.first][point.second] == 'A') answer++;
+                        intersectingPoints.emplace(point.first, point.second);
+                    }
+                }
+
+                curLetter = 0;
             }
         }
-
     }
 
     return answer;
