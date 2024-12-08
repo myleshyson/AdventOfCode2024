b42b29a8ada703b45d6242902d6460a9ef032810
Myles Hyson <myleshyson@ufl.edu>
2024-12-07 20:44:25 -0800

    support multi treading on question 6

--- 6.cpp
+++ 6.cpp
@@ -1,27 +1,40 @@
 #include <sstream>
 #include <map>
 #include <set>
+#include <utility>
 #include <vector>
 #include <iostream>
-#include <mutex>
-#include <thread>
+#include <future>
+#include <optional>
 #include "helpers.h"
+
+typedef std::pair<int, int> position;
+typedef std::pair<int, int> direction;
+typedef std::vector<std::vector<char> > content;
+typedef std::set<position> visited;
+typedef std::map<position, position> skip;
+typedef std::map<std::pair<int, int>, std::vector<std::pair<int, int> > > graph;
+
+struct Result {
+    std::optional<visited> visited;
+    explicit Result(std::optional<::visited> visited): visited(std::move(visited)) {};
+};
 
 const std::pair<int, int> LEFT = std::make_pair(0, -1);
 const std::pair<int, int> RIGHT = std::make_pair(0, 1);
 const std::pair<int, int> UP = std::make_pair(-1, 0);
 const std::pair<int, int> DOWN = std::make_pair(1, 0);
-const std::pair<int, int> DIRECTIONS[] = {LEFT, RIGHT, UP, DOWN};
 
 bool inBounds(const std::pair<int, int> position, const std::vector<std::vector<char> > &content) {
-    return position.first >= 0 && position.first < content.size() && position.second >= 0 && position.second < content[0].size();
+    return position.first >= 0 && position.first < content.size() && position.second >= 0 && position.second < content[
+               0].size();
 }
 
 bool isBoundry(const std::pair<int, int> position, const std::vector<std::vector<char> > &content) {
     return content[position.first][position.second] == '#';
 }
 
-std::pair<int, int> getNewDirection(const std::pair<int, int> &direction) {
+direction getNewDirection(const direction &direction) {
     if (direction == UP) return RIGHT;
     if (direction == DOWN) return LEFT;
     if (direction == RIGHT) return DOWN;
@@ -30,23 +43,29 @@ std::pair<int, int> getNewDirection(const std::pair<int, int> &direction) {
     return direction;
 }
 
-std::pair<int, int> getNewPosition(const std::pair<int, int> &position, const std::pair<int, int> &direction) {
+position getNewPosition(const position &position, const direction &direction) {
     return std::make_pair(position.first + direction.first, position.second + direction.second);
 }
 
-bool simulateRoute(std::pair<int, int> position, const std::vector<std::vector<char>> &content, std::set<std::pair<int, int>> &seen) {
-    std::pair<int, int> direction = UP;
-    std::set<std::tuple<std::pair<int, int>, std::pair<int, int>>> pointDirectionMap;
+Result simulateRoute(position position, const content &content, std::optional<::position> newObstruction = std::nullopt) {
+    visited seen;
+    direction direction = UP;
+    std::set<std::pair<::position, ::direction> > pointDirectionMap;
 
     while (inBounds(position, content)) {
-        if (pointDirectionMap.contains(std::make_tuple(position, direction))) return false;
+        if (pointDirectionMap.contains(std::make_pair(position, direction))) return Result(std::nullopt);
 
         seen.insert(position);
         pointDirectionMap.emplace(position, direction);
 
-        std::pair<int, int> newPosition = getNewPosition(position, direction);
+        ::position newPosition = getNewPosition(position, direction);
 
         if (inBounds(newPosition, content) && isBoundry(newPosition, content)) {
+            direction = getNewDirection(direction);
+            continue;
+        }
+
+        if (newObstruction && newPosition == newObstruction) {
             direction = getNewDirection(direction);
             continue;
         }
@@ -54,21 +73,16 @@ bool simulateRoute(std::pair<int, int> position, const std::vector<std::vector<c
         position = newPosition;
     }
 
-    return true;
-}
-
-void checkPoint(const std::pair<int, int> *point, const std::pair<int, int> *initialPosition, const std::vector<std::vector<char>> *content, std::mutex *answerMutex, int *answer) {
-    println("{}", *point);
+    return Result(seen);
 }
 
 int main() {
     std::ifstream file = get_input("6.txt");
     std::string line;
-    std::vector<std::vector<char> > content;
-    std::map<std::pair<int, int>, std::vector<std::pair<int, int> > > graph;
-    std::pair<int, int> position;
-    std::pair<int, int> initialPosition;
-    std::pair<int, int> direction = UP;
+    content content;
+    graph graph;
+    position initialPosition;
+    skip skipMap;
 
     while (getline(file, line)) {
         std::vector<char> characters;
@@ -78,31 +92,35 @@ int main() {
 
             if (line[i] == '^') {
                 initialPosition = std::make_pair(content.size(), i);
-                position = initialPosition;
             }
         }
 
         content.push_back(characters);
     }
 
-    std::set<std::pair<int, int>> seen;
-    simulateRoute(initialPosition, content, seen);
-    std::mutex answerMutex;
-    int answer = 0;
+    int answer[] = {0, 0};
+    auto thread = std::async(simulateRoute, initialPosition, std::ref(content), std::nullopt);
 
-    for (auto point : seen) {
-        if (point == initialPosition) continue;
+    if (thread.valid()) {
+        auto originalPath = thread.get().visited.value();
+        answer[0] = originalPath.size();
+        std::vector<std::future<Result>> threads;
 
-        checkPoint(&point, &initialPosition, &content, &answerMutex, &answer);
-
-        std::set<std::pair<int, int>> newSeen;
-
-        if (!simulateRoute(initialPosition, content, newSeen)) {
+        for (auto point: originalPath) {
+            if (point == initialPosition) continue;
+            threads.push_back(std::async(simulateRoute, initialPosition, content, point));
         }
 
+        for (auto &task: threads) {
+            if (task.valid()) {
+                auto result = task.get();
+                if (result.visited == std::nullopt) answer[1]++;
+            }
+        }
     }
 
-    std::cout << seen.size() << std::endl;
-    std::cout << answer << std::endl;
+
+    std::println("{}", answer);
+
     return 0;
 }
