4dc50ddc68a85ae800fde7bf58bcd247dc602cb2
Myles Hyson <myleshyson@ufl.edu>
2024-12-11 16:55:37 -0800

    Try to learn recursive approach. Check out googles benchmark lib.

--- 11.cpp
+++ 11.cpp
@@ -1,100 +1,63 @@
 #include <fstream>
 #include <sstream>
 #include <unordered_map>
+#include <map>
 #include <unordered_set>
 #include <iostream>
+#include <benchmark/benchmark.h>
 
 #include "helpers.h"
 
-double stoneCount(std::vector<double> stones, int blinks) {
-    std::unordered_map<double, double> map;
+long long countRecursive(long long stone, int blinks, std::map<std::pair<long long, int>, long long> &cache) {
+    auto key = std::make_pair(stone, blinks);
 
-    for (auto stone: stones) {
-        map[stone] = 1;
+    if (cache.contains(key)) {
+        return cache.at(key);
     }
 
-    for (int i = 0; i < blinks; i++) {
-        std::unordered_map<double, double> newMap = map;
+    long long count = 0;
 
-        auto size = static_cast<long long>(map.size());
-        auto setIterator = map.begin();
+    if (blinks == 0) {
+        count = 1;
+    } else if (stone == 0) {
+        count = countRecursive(1, blinks - 1, cache);
+    } else if (int digits = static_cast<int>(log10(stone)) + 1; digits % 2 == 0) {
+        std::string num = std::to_string(stone);
+        long long firstHalf = std::stoll(num.substr(0, digits / 2));
+        long long secondHalf = std::stoll(num.substr(digits / 2));
 
-        for (auto j = 0; j < size; j++) {
-            double stone = setIterator->first;
-
-            if (map[stone] <= 0) {
-                ++setIterator;
-                continue;
-            }
-
-            int digits = static_cast<int>(log10(stone)) + 1;
-
-            if (stone == 0) {
-                if (!newMap.contains(1)) {
-                    newMap.insert_or_assign(1, 0);
-                }
-
-                newMap[1] += map[0];
-            } else if (digits % 2 == 0) {
-                std::string num = std::to_string(stone);
-                double firstHalf = std::stod(num.substr(0, digits / 2));
-                double secondHalf = std::stod(num.substr(digits / 2));
-
-                if (!newMap.contains(firstHalf)) {
-                    newMap.insert_or_assign(firstHalf, 0);
-                }
-
-                if (!newMap.contains(secondHalf)) {
-                    newMap.insert_or_assign(secondHalf, 0);
-                }
-
-                newMap[firstHalf] += map[stone];
-                newMap[secondHalf] += map[stone];
-            } else {
-                double bigBoi = stone * 2024.0;
-
-                if (!newMap.contains(bigBoi)) {
-                    newMap.insert_or_assign(bigBoi, 0);
-                }
-
-                newMap[bigBoi] += map[stone];
-            }
-
-            newMap[stone] -= map[stone];
-
-            ++setIterator;
-        }
-        map = newMap;
+        count = countRecursive(firstHalf, blinks - 1, cache) + countRecursive(secondHalf, blinks - 1, cache);
+    } else {
+        count = countRecursive(stone * 2024, blinks - 1, cache);
     }
 
-   double answer = 0;
+    cache.insert_or_assign(key, count);
 
-    for (auto &pair: map) {
-        if (pair.second > 0) {
-            answer += pair.second;
-        }
-    }
-
-    return answer;
+    return cache.at(key);
 }
 
-int main() {
+static void recursive(benchmark::State &state) {
     std::ifstream file = get_input("11.txt");
     std::string line((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     std::stringstream ss(line);
     std::string string;
-    std::vector<double> stones;
+    std::vector<long long> stones;
+    std::map<std::pair<long long, int>, long long> cache;
 
     while (getline(ss, string, ' ')) {
-        stones.push_back(std::stod(string));
+        stones.push_back(std::stoll(string));
     }
 
-    double answer[] = {0, 0};
+    long long answer[] = {0, 0};
 
-    answer[0] = stoneCount(stones, 25);
-    answer[1] = stoneCount(stones, 75);
-
-    std::cout << std::format("{}", answer);
-
-    return 0;
+    for (auto _ : state) {
+        for (auto stone : stones) {
+            answer[0] += countRecursive(stone, 25, cache);
+            answer[1] += countRecursive(stone, 75, cache);
+        }
+    }
 }
+
+BENCHMARK(recursive);
+
+BENCHMARK_MAIN();
