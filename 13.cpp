d5a4f2b797d7ebadbb8665e6cac0180cd97387b0
Myles Hyson <myleshyson@ufl.edu>
2024-12-13 20:02:36 -0800

    simplified it. added a cite.

--- 13.cpp
+++ 13.cpp
@@ -1,21 +1,38 @@
 #include <regex>
 #include <sstream>
-#include <iostream>
-#include <functional>
 #include "helpers.h"
 
+/**
+ * All credit to this person. I'm just using this to learn more about how to work with linear algebra!
+ *
+ * @link https://github.com/PaigePalisade/AdventOfCode2024/blob/main/Solutions/day13part2.c
+ * @param ax
+ * @param ay
+ * @param bx
+ * @param by
+ * @param px
+ * @param py
+ * @return
+ */
 long long getTokens
-(std::pair<long long, long long> A, std::pair<long long, long long> B, std::pair<long long, long long> prize) {
+(
+    const long long ax,
+    const long long ay,
+    const long long bx,
+    const long long by,
+    const long long px,
+    const long long py
+) {
     long long tokens = 0;
-    long long numeratorX = prize.first * B.second - prize.second * B.first;
-    long long denominatorX = A.first * B.second - A.second * B.first;
+    const long long numeratorX = px * by - py * bx;
+    const long long denominatorX = ax * by - ay * bx;
 
-    long long numeratorY = prize.first * A.second - prize.second * A.first;
-    long long denominatorY = A.second * B.first - A.first * B.second;
+    const long long numeratorY = px * ay - py * ax;
+    const long long denominatorY = ay * bx - ax * by;
 
     if (numeratorX % denominatorX == 0 && numeratorY % denominatorY == 0) {
-        long long a = numeratorX / denominatorX;
-        long long b = numeratorY / denominatorY;
+        const long long a = numeratorX / denominatorX;
+        const long long b = numeratorY / denominatorY;
         tokens += 3 * a + b;
     }
 
@@ -23,52 +40,20 @@ long long getTokens
 }
 
 int main() {
-    std::ifstream input = get_input("13.txt");
-    std::string content((std::istreambuf_iterator(input)), std::istreambuf_iterator<char>());
-    std::stringstream ss(content);
-    std::string line;
-    std::string block;
-    std::vector<std::string> blocks;
-
-    while (getline(ss, line, '\n')) {
-        if (line.empty()) {
-            blocks.push_back(block);
-            block = "";
-            continue;
-        }
-
-        block += line + "\n";
-    }
-
-    blocks.push_back(block);
+    std::string projectPath = std::filesystem::path(__FILE__).parent_path();
+    std::string filename(projectPath + "/" + "13.txt");
+    FILE *f = fopen(filename.c_str(), "r");
 
     long long answer[] = {0, 0};
 
-    for (auto instructions: blocks) {
-        std::regex re(
-            R"(Button A: X\+([0-9]+), Y\+([0-9]+)\nButton B: X\+([0-9]+), Y\+([0-9]+)\nPrize: X\=([0-9]+), Y\=([0-9]+)\n)"
-        );
+    while (!feof(f)) {
+        long long ax, ay, bx, by, px, py;
+        fscanf(f, "Button A: X+%lld, Y+%lld ", &ax, &ay);
+        fscanf(f, "Button B: X+%lld, Y+%lld ", &bx, &by);
+        fscanf(f, "Prize: X=%lld, Y=%lld ", &px, &py);
 
-        auto matchesBegin = std::sregex_iterator(instructions.begin(), instructions.end(), re);
-        auto matchesEnd = std::sregex_iterator();
-
-        for (auto it = matchesBegin; it != matchesEnd; ++it) {
-            std::smatch match = *it;
-
-            auto ax = match[1];
-            auto ay = match[2];
-            auto bx = match[3];
-            auto by = match[4];
-            auto px = match[5];
-            auto py = match[6];
-
-            std::pair A(std::stoll(ax), std::stoll(ay));
-            std::pair B(std::stoll(bx), std::stoll(by));
-            std::pair destination(std::stoll(px), std::stoll(py));
-
-            answer[0] += getTokens(A, B, destination);
-            answer[1] += getTokens(A, B, {destination.first + 10000000000000, destination.second + 10000000000000});
-        }
+        answer[0] += getTokens(ax, ay, bx, by, px, py);
+        answer[1] += getTokens(ax, ay, bx, by, px + 10000000000000, py + 10000000000000);
     }
 
     std::println("{}", answer);
